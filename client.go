a8c2b1638092b4617f5afa4a133a6399fdfab4fc
Abdul Dakkak <abduld@wolfram.com>
2017-02-24 10:18:40 -0600

    continue work on client

--- client.go
+++ client.go
@@ -6,13 +6,20 @@ import (
 	"path/filepath"
 
 	"github.com/Unknwon/com"
+	"github.com/k0kubun/pp"
 	"github.com/pkg/errors"
+	"github.com/rai-project/broker"
+	"github.com/rai-project/broker/sqs"
 	"github.com/rai-project/ratelimit"
+	"github.com/rai-project/serializer/json"
 	"github.com/rai-project/user"
+	"github.com/rai-project/uuid"
 )
 
 type client struct {
-	options Options
+	options     Options
+	broker      broker.Broker
+	subscribers []broker.Subscriber
 }
 
 func New(opts ...Option) (*client, error) {
@@ -38,19 +45,90 @@ func New(opts ...Option) (*client, error) {
 		options.directory = cwd
 	}
 
+	return &client{
+		options: options,
+	}, nil
+}
+
+func (c *client) Validate() error {
+	options := c.options
+
 	// Authenticate user
 	if err := authenticate(options.profilePath); err != nil {
-		return nil, err
+		return err
 	}
 
 	buildFilePath := filepath.Join(options.directory, options.buildFileBaseName)
 	if !com.IsFile(buildFilePath) {
-		return nil, errors.Errorf("the build file [%v] does not exist", buildFilePath)
+		return errors.Errorf("the build file [%v] does not exist", buildFilePath)
 	}
 
 	if err := ratelimit.New(ratelimit.Limit(options.ratelimit)); err != nil {
-		return nil, err
+		return err
+	}
+	return nil
+}
+
+func (c *client) resultHandler(pub broker.Publication) error {
+	msg := pub.Message()
+	pp.Println(string(msg.Body))
+	return nil
+}
+
+func (c *client) Init() error {
+	brkr := sqs.New(
+		broker.Serializer(json.New()),
+	)
+	c.broker = brkr
+
+	id := uuid.NewV4()
+	err := brkr.Publish(
+		"rai",
+		&broker.Message{
+			ID: id,
+			Header: map[string]string{
+				"id": id,
+			},
+			Body: []byte("data"),
+		},
+	)
+	if err != nil {
+		return err
 	}
 
-	return nil, nil
+	subscriber, err := brkr.Subscribe(
+		"log-"+id,
+		c.resultHandler,
+		broker.AutoAck(true),
+	)
+	if err != nil {
+		return err
+	}
+
+	c.subscribers = append(c.subscribers, subscriber)
+
+	return nil
+}
+
+func (c *client) Connect() error {
+	return c.broker.Connect()
+}
+
+func (c *client) Disconnect() error {
+	for _, sub := range c.subscribers {
+		sub.Unsubscribe()
+	}
+	return c.broker.Disconnect()
+}
+
+func authenticate(profilePath string) error {
+	prof, err := user.NewProfile(profilePath)
+	if err != nil {
+		return err
+	}
+	ok := prof.Verify()
+	if !ok {
+		return errors.Errorf("cannot authenticate using the credentials in %v", profilePath)
+	}
+	return nil
 }
